57e161544e3615a92070c13d1cd727e1321ee0d9
alejandrodh <alejandro@digitalhouse.com>
2020-11-26 09:19:34 -0300

    buscador completo: todos y avanzado. Falta terminar la vista busqueda de personas.

---
+++ 13-httpsYQs/demo/js/results.js
@@ -0,0 +1,148 @@
+//1 Obtener la queryString
+let queryString = window.location.search;
+
+//2 Transformarla en un obejto literal
+let queryObject = new URLSearchParams(queryString);
+
+//3 Obtener EL dato para completar el end point.
+let searchData = queryObject.get('searchData'); //Cambie segun lo que tengo en la url
+let mediaType = queryObject.get('mediaType'); //Cambie segun lo que tengo en la url
+
+
+let apiKey = "e3f1ae8bae04c04c63af7b6996decd02"
+
+
+if(mediaType == "movie"){
+
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchData}&page=1&include_adult=false` //Viene de la API de TMDB
+
+    fetch(url)
+        .then(function (respuestas) {
+            return respuestas.json()
+        })
+        .then(function (data) {
+            //Aca operamos con los datos.
+            console.log(data);
+
+            let info = data.results //Esto es un array.
+            let movies = document.querySelector('.movies');
+            console.log(movies);
+
+            for (let i = 0; i < 4; i++) {
+                movies.innerHTML += `<article class="card-wrapper col-sm-3">
+                                    <div class="card my-3">
+                                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${info[i].title}</h5>
+                                                <p class="card-text">${info[i].overview}</p>
+                                                <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                                            </div>
+                                        </div>
+                                    </article>`
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+}
+
+if(mediaType == "tv"){
+
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchData}&page=1&include_adult=false` //Viene de la API de TMDB
+
+    fetch(url)
+        .then(function (respuestas) {
+            return respuestas.json()
+        })
+        .then(function (data) {
+            //Aca operamos con los datos.
+            console.log(data);
+
+            let info = data.results //Esto es un array.
+            let movies = document.querySelector('.movies');
+            console.log(movies);
+
+            for (let i = 0; i < 4; i++) {
+                movies.innerHTML += `<article class="card-wrapper col-sm-3">
+                                    <div class="card my-3">
+                                        <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${info[i].title}</h5>
+                                                <p class="card-text">${info[i].overview}</p>
+                                                <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                                            </div>
+                                        </div>
+                                    </article>`
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+if(mediaType == "person"){
+    //Completar
+}
+
+if(mediaType == "all"){
+    //fetch a multisearch
+
+    let url = ` https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${searchData}&page=1&include_adult=false` //Viene de la API de TMDB
+
+    fetch(url)
+        .then(function (respuestas) {
+            return respuestas.json()
+        })
+        .then(function (data) {
+            //Aca operamos con los datos.
+            console.log(data);
+
+            let info = data.results //Esto es un array.
+            let movies = document.querySelector('.movies');
+            console.log(movies);
+
+            for (let i = 0; i < info.length; i++) {
+                if(info[i].media_type == "movie"){
+                    movies.innerHTML += `<article class="card-wrapper col-sm-3">
+                                        <div class="card my-3">
+                                            <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">${info[i].title}</h5>
+                                                    <p class="card-text">${info[i].overview}</p>
+                                                    <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                                                </div>
+                                            </div>
+                                        </article>`
+                } else if (info[i].media_type == "tv"){
+                    movies.innerHTML += `<article class="card-wrapper col-sm-3">
+                                        <div class="card my-3">
+                                            <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">${info[i].original_name}</h5>
+                                                    <p class="card-text">${info[i].overview}</p>
+                                                    <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                                                </div>
+                                            </div>
+                                        </article>`
+                } else {
+                    movies.innerHTML += `<article class="card-wrapper col-sm-3">
+                                    <div class="card my-3">
+                                        <img src="https://image.tmdb.org/t/p/w500${info[i].profile_path}" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${info[i].name}</h5>
+                                                <p class="card-text"> Popularidad: ${info[i].popularity}</p>
+                                                <a href="movie.html?id=${info[i].id}" class="btn btn-primary">Ver más</a>
+                                            </div>
+                                        </div>
+                                    </article>`
+                }
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
